ea9c48a695f869a4b07dbc419322774c00dd4073
felix <felix.reidl@gmail.com>
2020-08-03 23:34:47 +0100

    Components work

--- src/graph.rs
+++ src/graph.rs
@@ -4,37 +4,48 @@ use crate::iterators::*;
 // use iterators::EdgeIterator
 
 pub type Vertex = u32;
-
+pub type VertexSet = FnvHashSet<Vertex>;
 
 #[derive(Debug)]
 pub struct Graph {
-    adj: FnvHashMap<Vertex, FnvHashSet<Vertex>>,
+    adj: FnvHashMap<Vertex, VertexSet>,
     degs: FnvHashMap<Vertex, u32>,
     _m: u64
 }
 
 impl Graph {
-    fn new() -> Graph {
+    pub fn new() -> Graph {
         Graph{adj: FnvHashMap::default(),
               degs: FnvHashMap::default(),
               _m: 0}
     }
 
     /*
-        Basic properties
+        Basic properties and queries
     */
-    fn num_vertices(&self) -> usize {
+    pub fn num_vertices(&self) -> usize {
         self.adj.len()
     }
 
-    fn num_edges(&self) -> u64 {
+    pub fn num_edges(&self) -> u64 {
         self._m
+    }
+
+    pub fn adjacent(&self, u:Vertex, v:Vertex) -> bool {
+        match self.adj.get(&u) {
+            Some(N) => N.contains(&v),
+            _ => false
+        }
+    }
+
+    pub fn degree(&self, u:Vertex) -> u32 {
+        *self.degs.get(&u).unwrap_or(&0)
     }
 
     /*
         Iteration and access
     */
-    fn contains(&mut self, u:Vertex) -> bool {
+    pub fn contains(&mut self, u:Vertex) -> bool {
         self.adj.contains_key(&u)
     }
 
@@ -50,6 +61,9 @@ impl Graph {
         NIterator::new(self)
     }
 
+    /*
+        Neighbourhood methods
+    */
     pub fn neighbours(&self, u:Vertex) -> NVertexIterator {
         match self.adj.get(&u) {
             Some(N) => N.iter(),
@@ -57,17 +71,54 @@ impl Graph {
         }
     }
 
+    pub fn neighbourhood<I>(&self, it:I) -> VertexSet where I: Iterator<Item=Vertex>  {
+        let mut res = FnvHashSet::default();
+        let centers:VertexSet = it.collect();
+
+        for &v in &centers {
+            res.extend(self.neighbours(v));
+        }
+
+        res.retain(|&u| !centers.contains(&u));
+        return res
+    }
+
+    pub fn closed_neighbourhood<I>(&self, it:I) -> VertexSet where I: Iterator<Item=Vertex>  {
+        let mut res = FnvHashSet::default();
+        let centers:VertexSet = it.collect();
+
+        for &v in &centers {
+            res.extend(self.neighbours(v));
+        }
+
+        return res
+    }
+
+    pub fn r_neighbours<I>(&self, u:Vertex, r:u32) -> VertexSet where I: Iterator<Item=Vertex>  {
+        return self.r_neighbourhood([u].iter().cloned(), r)
+    }
+
+    pub fn r_neighbourhood<I>(&self, it:I, r:u32) -> VertexSet where I: Iterator<Item=Vertex>  {
+        let mut res:VertexSet = FnvHashSet::default();
+        res.extend(it);
+        for _ in 0..r {
+            let ext = self.closed_neighbourhood(res.iter().cloned());
+            res.extend(ext);
+        }
+        return res
+    }
+
     /*
         Modification
     */
-    fn add_vertex(&mut self, u:Vertex) {
+    pub fn add_vertex(&mut self, u:Vertex) {
         if !self.adj.contains_key(&u) {
             self.adj.insert(u, FnvHashSet::default());
             self.degs.insert(u, 0);
         }
     }
 
-    fn add_edge(&mut self, u:Vertex, v:Vertex) -> bool {
+    pub fn add_edge(&mut self, u:Vertex, v:Vertex) -> bool {
         self.add_vertex(u);
         self.add_vertex(v);
         if !self.adjacent(u, v) {
@@ -82,7 +133,7 @@ impl Graph {
         }
     }
 
-    fn remove_edge(&mut self, u:Vertex, v:Vertex) -> bool {
+    pub fn remove_edge(&mut self, u:Vertex, v:Vertex) -> bool {
         if self.adjacent(u, v) {
             self.adj.get_mut(&u).unwrap().remove(&v);
             self.adj.get_mut(&v).unwrap().remove(&u);
@@ -94,15 +145,24 @@ impl Graph {
             false
         }
     }
-    // fn remove_loops(&mut self) -> u64 {
-    //     let c = 0
-    //
-    //
-    //
-    //     c
-    // }
 
-    fn remove_node(&mut self, u:Vertex) -> bool {
+    pub fn remove_loops(&mut self) -> usize {
+        let mut cands = Vec::new();
+        for u in self.vertices().cloned() {
+            if self.adjacent(u, u) {
+                cands.push(u)
+            }
+        }
+
+        let c = cands.len();
+        for u in cands.into_iter() {
+            self.remove_edge(u, u);
+        }
+
+        return c
+    }
+
+    pub fn remove_node(&mut self, u:Vertex) -> bool {
         if !self.contains(u) {
             false
         } else {
@@ -119,21 +179,63 @@ impl Graph {
         }
     }
 
-    fn adjacent(&self, u:Vertex, v:Vertex) -> bool {
-        match self.adj.get(&u) {
-            Some(N) => N.contains(&v),
-            _ => false
+    pub fn subgraph<I>(&self, vertices:I) -> Graph where I: Iterator<Item=Vertex> {
+        let mut G = Graph::new();
+        let selected:VertexSet = vertices.collect();
+        for &v in &selected {
+            G.add_vertex(v);
+            let Nv:VertexSet = self.neighbours(v).cloned().collect();
+            for u in Nv.intersection(&selected) {
+                G.add_edge(*u, v);
+            }
         }
+
+        return G
     }
 
-    fn degree(&self, u:Vertex) -> u32 {
-        *self.degs.get(&u).unwrap_or(&0)
+    pub fn components(&self) -> Vec<VertexSet> {
+        let mut vertices:VertexSet = self.vertices().cloned().collect();
+        let mut comps = Vec::new();
+        while vertices.len() > 0 {
+            let mut comp = VertexSet::default();
+            let u = vertices.iter().cloned().next().unwrap();
+            vertices.remove(&u);
+            comp.insert(u);
+
+            loop {
+                let ext = self.neighbourhood(comp.iter().cloned());
+                if ext.is_empty() {
+                    break;
+                }
+                comp.extend(ext);
+            }
+            vertices.retain(|&u| !comp.contains(&u));
+
+            comps.push(comp);
+        }
+        comps
     }
+
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn components() {
+        let mut G = Graph::new();
+        let n:u32 = 10;
+        for i in 0..(n/2) {
+            G.add_edge(i, 5+i);
+        }
+
+        // assert_eq!(G.components().len(), G.edges().count());
+
+        for comp in G.components() {
+            println!("{:?}", comp);
+        }
+    }
 
     #[test]
     fn basic_iteration() {
@@ -144,7 +246,7 @@ mod test {
         }
 
         assert_eq!(G.edges().count(), (n/2) as usize);
-        assert_eq!(G.edges().count(), G.num_edges() as usize);        
+        assert_eq!(G.edges().count(), G.num_edges() as usize);
     }
 
     #[test]
