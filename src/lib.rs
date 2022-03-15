f34f3c86d038f8f4638d1a2fa83f2bc71b382670
Felix Reidl <felix.reidl@gmail.com>
2022-03-15 00:42:26 +0000

    Removed python bindings, will be done in different crate

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,6 @@
 #![allow(unused_imports)]
 #![allow(non_snake_case)]
 
-use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
-
 use std::iter::Map;
 
 pub mod graph;
@@ -14,18 +11,3 @@ pub mod ordgraph;
 
 pub mod iterators;
 pub mod parse;
-
-pub mod pygraph;
-pub mod pyordgraph;
-
-#[cfg(not(test))] // pyclass and pymethods break `cargo test`
-#[pymodule]
-fn graphbench(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<pygraph::PyGraph>()?;
-    m.add_class::<pygraph::PyVertexMapDegree>()?;
-    m.add_class::<pygraph::PyVertexMapBool>()?;
-    m.add_class::<pyordgraph::PyOrdGraph>()?;
-    // m.add_wrapped(wrap_pyfunction!(from_pid))?;
-
-    Ok(())
-}
