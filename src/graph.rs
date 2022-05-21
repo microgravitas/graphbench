362b94ea6d2a8abc21a5ef918b3d4234654a474b
Felix <felix.reidl@gmail.com>
2022-05-21 15:00:14 +0100

    Adds Graph::add_vertices and Graph::add_edges

--- src/graph.rs
+++ src/graph.rs
@@ -192,6 +192,27 @@ pub trait MutableGraph: Graph{
 
     fn remove_edge(&mut self, u: &Vertex, v: &Vertex) -> bool;
 
+
+    fn add_vertices(&mut self, it: impl Iterator<Item=Vertex>) -> u32 {
+        let mut count = 0;
+        for v in it {
+            if self.add_vertex(&v) {
+                count += 1;
+            }
+        }
+        count
+    }
+
+    fn add_edges(&mut self, it: impl Iterator<Item=Edge>) -> u32 {
+        let mut count = 0;
+        for (u,v) in it {
+            if self.add_edge(&u, &v) {
+                count += 1;
+            }
+        }
+        count
+    }
+
     fn remove_loops(&mut self) -> usize {
         let mut cands = Vec::new();
         for u in self.vertices() {
